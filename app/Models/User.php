931dbe0423dea731747c7ae40542904a05bcaddd
Mouhsine Rouaki <houtm27@gmail.com>
2026-01-12 12:52:35 +0100

    ajout : ajout de abstract class user

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,21 @@ abstract class User{
     protected ?string $prenom_user;
     protected ?string $email_user;
     protected ?string $password_user;
+    protected ?string $passwordHashe_user;
     protected ?string $phone_user;
-     
+    protected ?string $role ;
+    
+    public function __construct($id ,$nom , $prenom , $email , $password , $phone , $role){
+        $this->id_user = $id;
+        $this->nom_user= $nom;
+        $this->prenom_user= $prenom;
+        $this->email_user= $email;
+        $this->password_user= $password;
+        $this->phone_user= $phone;
+        $this->role= $role;
+        $this->passwordHashe_user= password_hash($password,PASSWORD_DEFAULT);
+    }
+    
+
     
 }
