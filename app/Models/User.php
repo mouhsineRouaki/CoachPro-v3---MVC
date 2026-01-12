eeb2b859c39d43ce482afed3fb1e0d570b85b15f
Mouhsine Rouaki <houtm27@gmail.com>
2026-01-12 12:22:57 +0100

    ajout : ajout de class user et ajout de linterface imodifiable profiel

---
+++ app/Models/User.php
@@ -0,0 +1,11 @@
+<?php 
+abstract class User{
+    protected ?int $id_user ;
+    protected ?string $nom_user;
+    protected ?string $prenom_user;
+    protected ?string $email_user;
+    protected ?string $password_user;
+    protected ?string $phone_user;
+     
+    
+}
